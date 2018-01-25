4cb9d579cfd36b8da3522801d54abe1fac9ebefb
gurufront <ruslan.gunder@gmail.com>
2018-01-25 13:57:26 +0200

    Start work

---
+++ js/script.js
@@ -0,0 +1,41 @@
+
+
+$(".filtr__item").click(function() {
+	$(".filtr__item").removeClass("filtr__item_active");
+	$(this).addClass("filtr__item_active");
+
+
+
+
+
+
+});
+
+
+
+$(".item_all").click(function() {
+	$(".task").fadeIn();
+});
+$(".item_active").click(function() {
+	$(".task_done").hide();
+	$(".task_active").fadeIn();
+});
+
+$(".item_done").click(function() {
+	$(".task_active").hide();
+	$(".task_done").fadeIn();
+});
+
+
+$(".task .task__status, .task .task__title, .task .task__btn_progress").click(function() {
+	if ($(this).parent().hasClass("task_active")) {
+		$(this).parent().addClass("task_done");
+		$(this).parent().removeClass("task_active");
+	} else {
+		$(this).parent().removeClass("task_done");
+		$(this).parent().addClass("task_active");		
+	}
+});
+$(".task .task__btn_delete").click(function() {
+	$(this).parent().remove();
+});
